b318d8a2cb6c634448ab6d80addcddefc1b36af4
thom <tom@tksfz.org>
2019-06-17 19:52:55 -0700

    Add builder spec

--- fixql-core/src/test/scala/com/iterable/graphql/BuilderSpec.scala
+++ fixql-core/src/test/scala/com/iterable/graphql/BuilderSpec.scala
@@ -1,33 +1,83 @@
 package com.iterable.graphql
 
 import com.iterable.graphql.compiler.{QueryMappings, QueryReducer}
-import graphql.Scalars
+import graphql.Scalars._
 import graphql.schema.GraphQLList.list
-import graphql.schema.{GraphQLObjectType, GraphQLSchema}
-import org.scalatest.FlatSpec
+import graphql.schema.idl.SchemaPrinter
+import graphql.schema.{GraphQLObjectType, GraphQLSchema, GraphQLTypeReference}
+import org.scalatest.{FlatSpec, Matchers}
 
-class BuilderSpec extends FlatSpec with SchemaAndMappingsMutableBuilderDsl with SchemaDsl {
+class BuilderSpec extends FlatSpec with SchemaAndMappingsMutableBuilderDsl with SchemaDsl with Matchers {
 
-  class Root {
-    def schemaAndMappings2: (GraphQLSchema, QueryMappings) = {
-      schemaAndMappings { implicit schema => implicit mappings =>
-        queryType("Query") { implicit obj =>
-          field("humans", list(humanType)) ~> null
-
-          lazy val humanType =
-            objectType("Human") { implicit obj =>
-              field("name", Scalars.GraphQLString) ~> null
-            }
-        }
+  def schemaAndMappings2: (GraphQLSchema, QueryMappings) = {
+    schemaAndMappings { implicit schema => implicit mappings =>
+      queryType("QueryType") { implicit obj =>
+        lazy val humanType: GraphQLObjectType = humanMappings(droidType).include
+        lazy val droidType: GraphQLObjectType = droidMappings(humanType).include
+        droidType
       }
     }
   }
 
-  "builder" should "build" in {
-    WithinQueryType { implicit builder => implicit mappings =>
-      field("humans", Scalars.GraphQLString) ~> QueryReducer.jsObjects { null }
+  def humanMappings(droidType: GraphQLObjectType) = WithinQueryType { implicit builder => implicit mappings =>
+    field("humans", list(humanType)) ~> QueryReducer.jsObjects { null }
 
-
+    lazy val humanType = objectType("Human") { implicit obj =>
+      field("id", GraphQLID) ~> null
+      field("name", GraphQLString) ~> null
+      field("friends", list(droidType)) ~> null
+      field("homePlanet", GraphQLString) ~> null
     }
+    humanType
   }
+
+  def droidMappings(humanType: GraphQLObjectType) = WithinQueryType { implicit builder => implicit mappings =>
+    field("droids", list(droidType)) ~> QueryReducer.jsObjects { null }
+
+    lazy val droidType = objectType("Droid") { implicit obj =>
+      field("id", GraphQLID) ~> null
+      field("name", GraphQLString) ~> null
+      field("friends", list(humanType)) ~> null
+      field("primaryFunction", GraphQLString) ~> null
+    }
+    droidType
+  }
+
+
+  "builder" should "build" in {
+    val (schema, mappings) = schemaAndMappings2
+    val knownSchema = FromGraphQLJava.parseSchema(starWarsSchema)
+    //schema shouldEqual knownSchema
+
+    val printer = new SchemaPrinter(SchemaPrinter.Options.defaultOptions())
+    println(printer.print(schema))
+    println(printer.print(knownSchema))
+    printer.print(schema) shouldEqual printer.print(knownSchema)
+  }
+
+  val starWarsSchema =
+    """
+      |    schema {
+      |        query: QueryType
+      |    }
+      |
+      |    type QueryType {
+      |        humans: [Human]
+      |        droids: [Droid]
+      |    }
+      |
+      |    type Human {
+      |        id: ID!
+      |        name: String!
+      |        friends: [Droid]
+      |        homePlanet: String
+      |    }
+      |
+      |    type Droid {
+      |        id: ID!
+      |        name: String!
+      |        friends: [Human]
+      |        primaryFunction: String
+      |    }
+    """.stripMargin
 }
