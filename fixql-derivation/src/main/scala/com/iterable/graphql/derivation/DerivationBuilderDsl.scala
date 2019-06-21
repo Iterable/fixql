38596eb2cf84402dcdb1336d773aec80fb24b720
thom <tom@tksfz.org>
2019-06-20 23:06:45 -0700

    Add DerivationBuilderDsl with a test too.

---
+++ fixql-derivation/src/main/scala/com/iterable/graphql/derivation/DerivationBuilderDsl.scala
@@ -0,0 +1,61 @@
+package com.iterable.graphql.derivation
+
+import com.iterable.graphql.{MutableMappingsBuilder, SchemaAndMappingsMutableBuilderDsl}
+import graphql.schema.{GraphQLObjectType, GraphQLOutputType}
+import shapeless.ops.hlist
+import shapeless.{HList, LabelledGeneric}
+import shapeless.ops.hlist.{ToTraversable, ZipWithKeys}
+import shapeless.ops.record.{Keys, SelectAll, ToMap}
+
+trait DerivationBuilderDsl extends SchemaAndMappingsMutableBuilderDsl {
+  self =>
+
+  protected final def derive[T]
+  (implicit obj: GraphQLObjectType.Builder)= {
+    new Derive[T](obj.build.getName)
+  }
+
+  class Derive[T](name: String)(implicit obj: GraphQLObjectType.Builder) {
+    def addFieldsAndMappings[L <: HList, O <: HList, MV <: HList, S <: HList, V <: HList, L2 <: HList, K <: HList]
+    (selections: S)
+    (implicit
+     gen: LabelledGeneric.Aux[T, L],
+     select: SelectAll.Aux[L, S, V],
+     zipped: ZipWithKeys.Aux[S, V, L2],
+     mapValues: MapValuesNull.Aux[ToGraphQLType.type, L2, MV],
+     toMap: ToMap.Aux[MV, _, GraphQLOutputType],
+    // Simply concatenate the necessary implicits for both functions, somewhat redundantly
+     mappings: MutableMappingsBuilder,
+     keys: Keys.Aux[L, K],
+     selectKeys: hlist.SelectAll[K, S],
+     set: ToTraversable.Aux[S, Set, Symbol]
+    )  = {
+      addFields(selections)
+      addMappings(selections)
+    }
+
+    def addFields[L <: HList, O <: HList, MV <: HList, S <: HList, V <: HList, L2 <: HList]
+    (selections: S)
+    (implicit
+     gen: LabelledGeneric.Aux[T, L],
+     select: SelectAll.Aux[L, S, V],
+     zipped: ZipWithKeys.Aux[S, V, L2],
+     mapValues: MapValuesNull.Aux[ToGraphQLType.type, L2, MV],
+     toMap: ToMap.Aux[MV, _, GraphQLOutputType]
+    )  = {
+      val derived = DeriveGraphQLType.derive[T](name).selected(selections)
+      obj.fields(derived.getFieldDefinitions)
+    }
+
+    def addMappings[L <: HList, K <: HList, S <: HList]
+    (selections: S)
+    (implicit mappings: MutableMappingsBuilder,
+     gen: LabelledGeneric.Aux[T, L],
+     keys: Keys.Aux[L, K],
+     select: hlist.SelectAll[K, S],
+     set: ToTraversable.Aux[S, Set, Symbol])= {
+      val derived = DeriveMappings.derive[T](name).selected(selections)
+      self.addMappings(derived)
+    }
+  }
+}
--- fixql-derivation/src/test/scala/com/iterable/graphql/derivation/IsGraphQLOutputTypeSpec.scala
+++ fixql-derivation/src/test/scala/com/iterable/graphql/derivation/IsGraphQLOutputTypeSpec.scala
@@ -7,13 +7,13 @@ import graphql.Scalars._
 import graphql.schema.idl.SchemaPrinter
 import io.circe.Json
 import org.scalatest.{AsyncFlatSpec, Matchers}
-import play.api.libs.json.{JsArray, JsObject, Json => PlayJson}
+import play.api.libs.json.{JsArray, JsNull, JsObject, JsString, Json => PlayJson}
 import shapeless.HNil
 import slick.dbio.DBIO
 import slick.jdbc.JdbcBackend
 
 class IsGraphQLOutputTypeSpec extends AsyncFlatSpec with Matchers
-  with SchemaAndMappingsMutableBuilderDsl with ReducerHelpers {
+  with SchemaAndMappingsMutableBuilderDsl with ReducerHelpers with DerivationBuilderDsl {
 
   case class Test(foo: String, bar: Int)
 
@@ -33,6 +33,8 @@ class IsGraphQLOutputTypeSpec extends AsyncFlatSpec with Matchers
   private val repo = new CharacterRepo
   private val slickDb = JdbcBackend.Database.forURL("jdbc:h2:mem:test", driver = "org.h2.Driver")
 
+  // TODO: add negative tests to verify query syntax validation fails when you
+  // specify fields that aren't in the case class
   "mappings derivation" should "generate mappings that execute correctly" in {
     val (schema, mappings) =
       schemaAndMappings { implicit builders =>
@@ -65,6 +67,43 @@ class IsGraphQLOutputTypeSpec extends AsyncFlatSpec with Matchers
         //"appearsIn" -> Seq("NEWHOPE", "EMPIRE", "JEDI"),
         //"homePlanet" -> "Tatooine"
       )
+    }
+  }
+
+  "derivation builder dsl" should "generate mappings that execute correctly" in {
+    val (schema, mappings) =
+      schemaAndMappings { implicit builders =>
+        withQueryType { implicit obj =>
+          field("humans", list(humanType)) ~> QueryReducer.jsObjects { _ =>
+            DBIO.successful(repo.getHumans(1000, 0).map(PlayJson.toJson(_).as[JsObject]))
+          }
+            .mergeResolveSubfields
+            .toTopLevelArray
+        }
+
+        lazy val humanType =
+          objectType("Human") { implicit obj =>
+            field("id", GraphQLID) ~> QueryReducer.mapped(_("id"))
+
+            import shapeless.syntax.singleton._
+            derive[Human].addFieldsAndMappings('name.narrow :: 'homePlanet.narrow :: HNil)
+          }
+
+        addMappings(standardMappings)
+      }
+
+    val queryStr = "{ humans { id name homePlanet } }"
+    val query = FromGraphQLJava.parseAndValidateQuery(schema, queryStr, Json.obj())
+    val dbio = Compiler.compile(FromGraphQLJava.toSchemaFunction(schema), query.get, mappings)
+    slickDb.run(dbio).map { queryResults =>
+      val arr = (queryResults \ "humans").as[JsArray]
+      arr.value.size shouldEqual repo.getHumans(1000, 0).size
+      arr.value.head shouldEqual PlayJson.obj(
+        "id" -> "1000",
+        "name" -> "Luke Skywalker",
+        "homePlanet" -> "Tatooine",
+      )
+      arr.value.find(_("name") == JsString("Han Solo")).get.apply("homePlanet") shouldEqual JsNull
     }
   }
 }
