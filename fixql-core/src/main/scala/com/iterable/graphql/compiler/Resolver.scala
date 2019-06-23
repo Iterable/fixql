f2152acaa1eae7ee422cf87c108d4462fe459d0f
thom <tom@tksfz.org>
2019-06-22 21:21:57 -0700

    Comment

--- fixql-core/src/main/scala/com/iterable/graphql/compiler/Resolver.scala
+++ fixql-core/src/main/scala/com/iterable/graphql/compiler/Resolver.scala
@@ -10,7 +10,7 @@ trait Resolver[F[_], A] {
   /** As the GraphQL tutorial says, "You can think of each field in a GraphQL query as a function or method of the
     * previous type which returns the next type." (https://graphql.org/learn/execution/)
     *
-    * A non-batched `resolve` method would have signature JsObject => DBIO[JsValue].
+    * A non-batched `resolve` method would have signature JsObject => F[JsValue].
     * Since batching is more general and more performant, we use a batched
     * signature below.
     *
