0c17c46f928dceaedc13642d7be7f36ecdee6a56
@tksfz <thomkim+github@gmail.com>
2019-06-28 15:45:01 -0700

    Type ofType

--- fixql-core/src/main/scala/com/iterable/graphql/introspection/IntrospectionMappings.scala
+++ fixql-core/src/main/scala/com/iterable/graphql/introspection/IntrospectionMappings.scala
@@ -39,6 +39,7 @@ class IntrospectionMappings(graphqlSchema: GraphQLSchema) {
     case ObjectField("__Type", "name") => QueryReducer.mapped(_("name"))
     case ObjectField("__Type", "description") =>  QueryReducer.mapped(_("description"))
     case ObjectField("__Type", "fields") => QueryReducer.mapped(_("fields"))
+    case ObjectField("__Type", _) => QueryReducer.jsValues[F] { _ => F.pure(Seq(JsNull)) }.toTopLevelArray
     case ObjectField("__Field", fieldName) => QueryReducer.mapped(_(fieldName))
     case ObjectField("__InputValue", fieldName) => QueryReducer.mapped(_(fieldName))
   }
